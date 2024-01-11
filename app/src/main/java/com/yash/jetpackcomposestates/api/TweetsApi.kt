f2fa863e420b6a5ff7d0bc43ea9f1cf1dfa4ff70
Yashveer singh <yashveersingh30401@gmail.com>
2024-01-11 23:09:12 +0530

    Module Created

---
+++ app/src/main/java/com/yash/jetpackcomposestates/api/TweetsApi.kt
@@ -0,0 +1,17 @@
+package com.yash.jetpackcomposestates.api
+
+import com.yash.jetpackcomposestates.models.TweetListItem
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+
+interface TweetsApi {
+
+    @GET("/v3/b/659ed9bb1f5677401f1a5db8?meta=false")
+    suspend fun getTweets(@Header("X-JSON-Path") category:String):Response<List<TweetListItem>>
+
+    @GET("/v3/b/659ed9bb1f5677401f1a5db8?meta=false")
+    @Headers("X-JSON-Path : $..tweets[*].category")
+    suspend fun getCategories():Response<List<String>>
+}
---
+++ app/src/main/java/com/yash/jetpackcomposestates/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.yash.jetpackcomposestates.di
+
+import com.yash.jetpackcomposestates.api.TweetsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+
+    @Singleton
+    @Provides
+    fun providesRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://api.jsonbin.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesTweetsApi(retrofit: Retrofit):TweetsApi{
+        return retrofit.create(TweetsApi::class.java)
+    }
+}
